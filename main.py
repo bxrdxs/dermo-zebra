41a15b3f33a5b5d2bad604ea5c9903861246557e
bxrdxs <88906083+bxrdxs@users.noreply.github.com>
2023-03-10 19:18:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+#1Application heading
+st.title("Dermoverse Skin Cancer Detector")
+
+#1Brief summary of what the application does
+st.subheader("This application can classify potential skin cancer images into two classes, whether they are benign or malignant")
+
+#1Information of what kind of images should be uploaded.
+st.subheader("The images uploaded should be a close up of the suspect skin anomoly ")
+
+# Load the pre-trained model
+model = tf.keras.models.load_model('dermodev.h5')
+
+# Define the class labels
+class_labels = ['Malignant', 'Benign']
+
+# Define the Streamlit app
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+# Define the "Run Model" button
+if uploaded_file is not None:
+    run_model = st.button("Run Model")
+else:
+    run_model = False
+
+# Make a prediction on the uploaded image when the "Run Model" button is clicked
+if run_model:
+    image = Image.open(uploaded_file)
+    image = image.resize((224, 224))
+    image = np.array(image)
+    image = image.astype('float32') / 255.0
+    prediction = model.predict(np.expand_dims(image, axis=0))[0]
+    percentages = tf.nn.softmax(prediction) * 100
+
+    # Print the prediction
+    st.write("Prediction:")
+    for i, percentage in enumerate(percentages):
+        class_label = class_labels[i]
+        st.write(f"{class_label}: {percentage:.2f}%")
+
+
